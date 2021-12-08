10840cbfedea799d6b2e320803b04fb82900dc14
DominikBosnjak <dominik.bosnjak@cinnamon.agency>
2021-12-08 10:37:36 +0100

    adding final changes and enabling storing user data into cookie

--- server.js
+++ server.js
@@ -35,13 +35,13 @@ app.post('/login', (req, res)=> {
     console.log(payload)
     }
     verify()
-    then(()=> {
+    .then(()=> {
         res.cookie('session-token', token);
         res.send('success');
     }).catch(console.error)
 })
 
-app.get('/profile', (req, res) => {
+app.get('/profile', checkAuthenticated, (req, res) => {
     let user = req.user;
     res.render('profile', {user});
 })
@@ -53,6 +53,37 @@ app.get('/protectedroute', (req, res)=> {
 app.get('/', (req, res) => {
     res.render('index');
 })
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('session-token');
+    res.redirect('/login');
+})
+
+function checkAuthenticated(req, res, next){
+
+    let token = req.cookies['session-token'];
+
+    let user = {};
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        user.name = payload.name;
+        user.email = payload.email;
+        user.picture = payload.picture;
+      }
+      verify()
+      .then(()=>{
+          req.user = user;
+          next();
+      })
+      .catch(err=>{
+          res.redirect('/login')
+      })
+
+}
 
 app.listen(PORT, ()=> {
     console.log(`Server running on port ${PORT}`);
